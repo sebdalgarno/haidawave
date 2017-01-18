1e23470a18409b78d9c1973f3dc2b031ce6685d7
joethorley <joe@poissonconsulting.ca>
2017-01-18 11:02:07 -0800

    add cumshewa_wind

---
+++ R/data.R
@@ -0,0 +1,14 @@
+#' Cumshewa Wind
+#'
+#' A tbl of the hourly wind direction and speed at Cumshewa Head 2012 to 2016.
+#'
+#'
+#' High/Low Tide Predictions from \url{http://tidesandcurrents.noaa.gov/tide_predictions.html}.
+#'
+#' @format A tbl data frame:
+#' \describe{
+#'   \item{Station}{The station name (chr).}
+#'   \item{DateTime}{The date time (time).}
+#'   \item{MLLW}{The tide height in m (dbl).}
+#' }
+"cumshewa_wind"
