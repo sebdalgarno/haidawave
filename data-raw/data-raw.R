cf7c7863fae433713da8df48a6bc184b3482b3f8
joethorley <joe@poissonconsulting.ca>
2017-01-24 16:13:39 -0800

    set cumshewa_wind timezone to PST

--- data-raw/data-raw.R
+++ data-raw/data-raw.R
@@ -1,12 +1,15 @@
 library(magrittr)
 library(dplyr)
 library(readr)
+library(lubridate)
 library(devtools)
 
 rm(list = ls())
 
 cumshewa_wind <- read_csv('data-raw/wind-cumshewa-1216.csv')
 laskeek_fetch <- read_csv('data-raw/laskeekbay-fetch-5.csv')
+
+cumshewa_wind$DateTime %<>% force_tz(tzone = "Etc/GMT+8")
 
 laskeek_fetch %<>% dplyr::select(PointID = PointID,
                           Long = Long,
